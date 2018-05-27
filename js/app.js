c08ad50f6bde95b8b1985252ddeecda8442fd801
Chu Kim Thang <chukimthang94@gmail.com>
2018-05-27 16:04:38 +0700

    Data Binding

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Chu Kim Thang",
-    job: "Information Technology"
+    job: "Information Technology",
+    website: "https://sochitieu.herokuapp.com",
+    websiteTag: "<a href='https://www.youtube.com'>Link youtube</a>"
   },
   methods: {
     greet:function(time) {
