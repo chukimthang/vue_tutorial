6e97b4700fa0426f301afe8d505a598002e0af4d
Chu Kim Thang <chukimthang94@gmail.com>
2018-05-27 15:39:47 +0700

    Data & Methods

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Chu Kim Thang"
+    name: "Chu Kim Thang",
+    job: "Information Technology"
+  },
+  methods: {
+    greet:function(time) {
+      return "Good " + time + " " + this.name;
+    }
   }
 });
