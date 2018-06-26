36cf91dceb43a8f121723a38f688956acfb3a5f1
Chu Kim Thang <chukimthang94@gmail.com>
2018-06-26 17:11:07 +0700

    Looping with v-for

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 new Vue({
   el: "#vue-app",
   data: {
-    error: false,
-    success: false
+    characters: ["Mario", "Luigi", "Yoshi", "Bowser"],
+    ninjas: [
+      {name: "Thang", age: 24},
+      {name: "Loi", age: 22},
+      {name: "Phu", age: 21}
+    ]
   },
+
+  methods: {
+
+  },
+
   computed: {
   }
 });
