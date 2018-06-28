a2f5acbc11e212dc8a675aba0ba74f033f5f9a1e
Chu Kim Thang <chukimthang94@gmail.com>
2018-06-28 13:34:40 +0700

    Fix

--- vuejs-playlist/src/components/Ninjas.vue
+++ vuejs-playlist/src/components/Ninjas.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="ninjas">
     <ul>
-      <li v-for="ninja in ninjas" v-on-click="ninja.show = !ninja.show">
+      <li v-for="ninja in ninjas" v-on:click="ninja.show = !ninja.show">
         <h2>{{ ninja.name }}</h2>
         <h3 v-show="ninja.show">{{ ninja.age }}</h3>
       </li>
